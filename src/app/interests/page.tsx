6963469e3707cfe1d9747bdab537654fb054ad46
Stephen Zacarias <stephenzacarias2@gmail.com>
2024-08-14 21:15:09 -0700

    Create new routes

---
+++ src/app/interests/page.tsx
@@ -0,0 +1,5 @@
+const Interests: React.FC = () => {
+  return <></>;
+};
+
+export default Interests;
---
+++ src/components/app/layout/main-container.tsx
@@ -0,0 +1,5 @@
+const MainContainer: React.FC = () => {
+  return <></>;
+};
+
+export default MainContainer;

dc68cc9a82bbb6de95db442f83b58bb0c8f39bbd
Stephen Zacarias <stephenzacarias2@gmail.com>
2024-09-29 11:59:26 -0700

    fix merge conflict

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,20 +5,10 @@ import { NavBar } from '../components/app/nav-bar';
 import { Footer } from '../components/app/footer';
 import { MainContainer } from '../components/app/layout/main-container';
 import './globals.css';
-import type { Metadata } from 'next';
-import { Roboto_Mono } from 'next/font/google';
-import { TopScrollBar } from '../components/app/top-scroll-bar';
-import { NavBar } from '../components/app/nav-bar';
-import { Footer } from '../components/app/footer';
-import { MainContainer } from '../components/app/layout/main-container';
-import './globals.css';
 
-const robotoMono = Roboto_Mono({ weight: '400', subsets: ['latin'] });
 const robotoMono = Roboto_Mono({ weight: '400', subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Stephen Zacarias | Portfolio',
-  description: 'My personal portfolio!',
   title: 'Stephen Zacarias | Portfolio',
   description: 'My personal portfolio!',
 };

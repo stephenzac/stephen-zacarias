ac2427d6d4971d5e004410c301775f8a2ecca38a
Stephen Zacarias <stephenzacarias2@gmail.com>
2024-09-29 11:56:49 -0700

    fix merge conflict

--- src/components/projects/project-skill.tsx
+++ src/components/projects/project-skill.tsx
@@ -7,13 +7,3 @@ export const ProjectSkill: React.FC<ProjectSkillProps> = ({ skill }) => (
     {skill}
   </div>
 );
-
-interface ProjectSkillProps {
-  skill: string;
-}
-
-export const ProjectSkill: React.FC<ProjectSkillProps> = ({ skill }) => (
-  <div className="text-sm bg-accent text-white rounded-lg text-nowrap py-1 px-3 border-2 border-gray-300 hover:border-white hover:scale-110 transition-transform">
-    {skill}
-  </div>
-);

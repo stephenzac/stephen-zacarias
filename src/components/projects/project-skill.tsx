b39716f599bc0d64742b2cc288987c7f1ab2af87
stephenzac <stephenzacarias2@gmail.com>
2024-12-15 13:49:56 -0800

    move to new project div

--- src/components/projects/project-skill.tsx
+++
@@ -1,9 +0,0 @@
-interface ProjectSkillProps {
-  children: string;
-}
-
-export const ProjectSkill: React.FC<ProjectSkillProps> = ({ children }) => (
-  <div className='text-sm bg-accent hover:bg-yellow-500 text-white rounded-lg text-nowrap py-1 px-3 border-2 border-gray-300 hover:border-white hover:scale-110 transition-transform'>
-    {children}
-  </div>
-);

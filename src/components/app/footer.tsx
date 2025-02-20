971c235d76d8bf486b35a602af38aa542aa97a5a
stephenzac <stephenzacarias2@gmail.com>
2025-02-20 12:41:41 -0800

    update copyright year

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer: React.FC = () => (
   <footer className='flex flex-col items-start w-full text-sm text-balance text-gray-400'>
-    <p>&copy; Stephen Zacarias 2024</p>
+    <p>&copy; Stephen Zacarias 2025</p>
     <p>
       Built with{' '}
       <span className='text-white'>

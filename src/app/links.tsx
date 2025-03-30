e8336efa0d8f43c8ac4f0beff088a38943ce6168
stephenzac <stephenzacarias2@gmail.com>
2025-03-30 04:02:13 -0700

    make icon sizes consistent, add color on hover

--- src/app/links.tsx
+++ src/app/links.tsx
@@ -3,21 +3,28 @@ import { FiYoutube } from 'react-icons/fi';
 import { LiaGithubSquare } from 'react-icons/lia';
 
 export const Links: React.FC = () => (
-  <div className='flex gap-4 text-4xl mt-6'>
+  <div className='flex gap-4 text-3xl mt-6 items-center'>
     <a
       href='https://linkedin.com/in/stephen-zac'
       target='_blank'
       rel='noreferrer'
+      className='hover:text-accent transition-colors duration-200'
     >
       <AiOutlineLinkedin />
     </a>
-    <a href='https://github.com/stephenzac' target='_blank' rel='noreferrer'>
+    <a
+      href='https://github.com/stephenzac'
+      target='_blank'
+      rel='noreferrer'
+      className='text-[36px] hover:text-accent transition-colors duration-200'
+    >
       <LiaGithubSquare />
     </a>
     <a
       href='https://open.spotify.com/user/cheesehugger1'
       target='_blank'
       rel='noreferrer'
+      className='hover:text-accent transition-colors duration-200'
     >
       <AiOutlineSpotify />
     </a>
@@ -25,6 +32,7 @@ export const Links: React.FC = () => (
       href='https://www.youtube.com/@Cheesehuggerz'
       target='_blank'
       rel='noreferrer'
+      className='hover:text-accent transition-colors duration-200'
     >
       <FiYoutube />
     </a>

e22d2d4756c17f86539c2fed928061ba5481a21b
Stephen Zacarias <95317322+stephenzac@users.noreply.github.com>
2025-04-22 16:06:23 -0700

    create sitemap (#8)

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://stephenzacarias.com',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
